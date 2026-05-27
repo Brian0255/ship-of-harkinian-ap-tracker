90fa0a2a0bde24ad2bd701a51a79d415f01a26ef
Brian0255 <brian.neveu2@gmail.com>
2026-05-27 17:06:06 -0400

    update location to handle sequence breaking, also add helper method to remove event item

--- scripts/logic/base_classes/location.lua
+++ scripts/logic/base_classes/location.lua
@@ -23,12 +23,31 @@ function Location.new(name, parent_region, access_rule)
 end
 
 function Location:can_reach(state)
-    assert(self.parent_region, string.format("called can_reach on a Location %s with no parent_region", self.name))
-    return self.parent_region:can_reach(state) and self.access_rule(state)
+    --assert(self.parent_region, string.format("called can_reach on a Location %s with no parent_region", self.name))
+    local parent_access_level = self.parent_region:can_reach(state)
+    if parent_access_level == ACCESS_NONE then
+        return ACCESS_NONE
+    end
+    if self.access_rule(state) then
+        return parent_access_level
+    end
+
+    local was_glitched = state.is_glitched
+    state:set_glitched_state(true)
+    if self.access_rule(state) then
+        state:set_glitched_state(was_glitched)
+        return ACCESS_SEQUENCEBREAK
+    end
+    state:set_glitched_state(was_glitched)
+    return ACCESS_NONE
 end
 
 function Location:set_event_item(event_item)
     self.event_item = event_item
+end
+
+function Location:remove_event_item()
+    self.event_item = nil
 end
 
 return Location
